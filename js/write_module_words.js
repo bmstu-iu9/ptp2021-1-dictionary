d486ed5a770d93d047b6477e521e72015c220f51
polinasam12 <sam53351@yandex.ru>
2021-07-13 10:24:48 +0300

    Создание card_head и card_body перенесено в html

--- js/write_module_words.js
+++ js/write_module_words.js
@@ -1,13 +1,7 @@
 function write_module_words(n) {
-    let card = document.querySelector(".card");
-    let card_head = document.createElement("div");
+    let card_head = document.querySelector(".card-header");
+    let card_body = document.querySelector(".card-body");
     card_head.innerHTML = "Module " + n;
-    card_head.className = "card-header";
-    card_head.classList.add("text-center");
-    card.appendChild(card_head);
-    let card_body = document.createElement("div");
-    card_body.className = "card-body";
-    card.appendChild(card_body);
     for (let i = 0; i < words.length; i++) {
         if (words[i].module == n) {
             let word_translation = document.createElement("a");
