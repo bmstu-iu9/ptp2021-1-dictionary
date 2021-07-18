0b7f566d9d9aec6260c7c73ed1c9fff3c9d43ba2
Lasadaf <86784572+Lasadaf@users.noreply.github.com>
2021-07-18 03:04:23 +0300

    делает всё, кроме сложных предложений

--- js/translation.js
+++ js/translation.js
@@ -1,10 +1,52 @@
-var url = new URL(document.location.href);
-document.getElementById('inpt').value = "";
-var task = url.searchParams.get('task');
-var lang = url.searchParams.get('lang');
-var rb = document.querySelector('.checkAnswer');
-if (task == "translate") { rb.style.display = 'none'; }
-newWord();
+function trim(word) {
+    var str = "";
+    var i = 0;
+    for (i; word[i] == ' ' || word[i] == '\n' || word[i] == '\t'; i += 1) {}
+    var j = word.length - 1;
+    for (j; word[j] == ' ' || word[j] == '\n' || word[j] == '\t'; j -= 1) {}
+    for (i; i <= j; i += 1) {
+        str += word[i];
+    }
+    return str;
+}
+
+function parse(str) {
+    var i = 0;
+    var answ = new Array();
+    var wrd = "";
+    for (i; i < str.length; i += 1) {
+        if (str[i] == ',' || str[i] == '/') {
+            answ.push(trim(wrd));
+            wrd = "";
+            continue;
+        }
+        if (str[i] == '(' && (i == 0 || str[i - 1] != ' ')) {
+            var wrd2 = wrd;
+            i += 1;
+            for (i; str[i] != ')' ; i++) {
+                wrd2 += str[i];
+            }
+            i += 1;
+            for (i; i < str.length && str[i] != ',' && str[i] != '/' ; i++) {
+                wrd += str[i];
+                wrd2 += str[i];
+            }
+            if (i!= str.length) {
+                answ.push(trim(wrd));
+                wrd = "";
+            }
+            answ.push(trim(wrd2));
+        } else {
+            if (str[i] == '(') {
+                for (i; str[i] != ')' ; i++) {}
+            } else {
+                wrd += str[i];
+            }
+        }
+    }
+    answ.push(trim(wrd));
+    return answ;
+}
 
 function correct(want) {
     var str = "";
@@ -14,7 +56,6 @@ function correct(want) {
     }
     return str;
 }
-
 
 function checkAnswer(want) {
     var got = document.getElementById('inpt').value;
@@ -33,30 +74,32 @@ function checkAnswer(want) {
 }
 
 function newWord() {
-    var rand = Math.floor(Math.random() * data.length);
+    var rand = Math.floor(Math.random() * words.length);
     var word, trans;
     if (lang == "eng") {
-        word = data[rand].word + " (" + data[rand].pos + ")";
-        trans = data[rand].translation;
+        word = trim(words[rand].word) + " (" + trim(words[rand].pos) + ")";
+        trans = trim(words[rand].translation);
     } else {
-        word = data[rand].translation;
-        trans = data[rand].word;
+        word = trim(words[rand].translation);
+        trans = trim(words[rand].word);
     }
+    //alert(word);
     document.getElementById("exercise").textContent = word;
-    var answ = new Array();
-    var str = "";
-    for (var i = 0 ; i < trans.length ; i++) {
-        if (trans[i] == ',') {
-            answ.push(str);
-            str = "";
-            i+=2;
-        }
-        str += trans[i];
-        //document.writeln(str);
-    }
-    answ.push(str);
+    var answ = parse(trans);
     document.getElementById('btn1').onclick = function () {
         checkAnswer(answ);
     }
+    document.getElementById('inpt').addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) {
+            checkAnswer(answ);
+        }
+    });
 }
 
+var url = new URL(document.location.href);
+document.getElementById('inpt').value = "";
+var task = url.searchParams.get('task');
+var lang = url.searchParams.get('lang');
+var rb = document.querySelector('.checkAnswer');
+if (task == "translate") { rb.style.display = 'none'; }
+newWord();
