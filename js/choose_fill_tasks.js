7bf48a4ef0b9289eddf2420343768db9ef221698
polinasam12 <sam53351@yandex.ru>
2021-07-20 07:18:08 +0300

    Добавлено рассмотрение случая, когда слова из словосочетания идут в примере не подряд или изменяется их форма

--- js/choose_fill_tasks.js
+++ js/choose_fill_tasks.js
@@ -73,6 +73,50 @@ function fill_task() {
     if (s.search(re) != -1) {
         word = s.match(re)[0];
         s = s.replace(re, "______");
+    } else {
+        w1 = "";
+        for (let i = 0; i < word_arr.length - 1; i++) {
+            w1 += ("\\b" + word_arr[i] + "[a-z]*\\b|");
+            if (word_arr[i][word_arr[i].length - 1] == "y") {
+                w1 += ("\\b" + word_arr[i].slice(0, word_arr[i].length - 1) + "ied\\b|");
+            }
+        }
+        w1 += "\\b" + word_arr[word_arr.length - 1] + "[a-z]*\\b"
+        if (word_arr[word_arr.length - 1][word_arr[word_arr.length - 1].length - 1] == "y") {
+            w1 += ("|\\b" + word_arr[word_arr.length - 1].slice(0, word_arr[word_arr.length - 1].length - 1) + "ied\\b");
+        }
+        re1 = new RegExp(w1, "gi");
+        s_words = Array.from(s.matchAll(re1));
+        s_words1 = [];
+        corr_ans = "";
+        for (let i = 0; i < s_words.length - word_arr.length + 1; i++) {
+            k = 0;
+            for (let j = 0; j < word_arr.length; j++) {
+                w2 = "\\b" + word_arr[j] + "[a-z]*\\b";
+                if (word_arr[j][word_arr[j].length - 1] == "y") {
+                    w2 += ("|\\b" + word_arr[j].slice(0, word_arr[j].length - 1) + "ied\\b");
+                }
+                let re2 = new RegExp(w2, "i");
+                if (s_words[i + j][0].search(re2) != -1) {
+                    k++;
+                }
+            }
+            if (k == word_arr.length) {
+                for (let j = 0; j < word_arr.length; j++) {
+                    s_words1.push(s_words[i + j]);
+                    corr_ans += (s_words[i + j][0] + " ");
+                }
+            }
+        }
+        corr_ans = corr_ans.trim();
+        let x = s_words1[0].index;
+        let y = s_words1[s_words1.length - 1].index + s_words1[s_words1.length - 1][0].length;
+        s1 = s.slice(x, y);
+        s1 = s1.replace(re1, "______");
+        s = s.slice(0, x) + s1 + s.slice(y, s.length);
+        re3 = /(______\s)+______/g;
+        s = s.replace(re3, "______");
+        word = corr_ans;
     }
     word = word.split(" ");
     for (let i = 0; i < word.length; i++) {
