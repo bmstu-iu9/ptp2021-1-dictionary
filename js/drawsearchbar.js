63d554255c1e5c764f7ddeb5b42b1863469078b0
Lasadaf <86784572+Lasadaf@users.noreply.github.com>
2021-07-22 01:07:50 +0300

    скрипт для отрисовки шапки

---
+++ js/drawsearchbar.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = process;
+url = document.location.origin + "/include/stroka.html";
+//alert(url);
+xhr.open("GET", url, true);
+xhr.send();
+function process()
+{
+  if (xhr.readyState == 4) {
+    var resp = xhr.responseText;
+    let div=document.createElement('div');
+    div.innerHTML=resp.toString();
+    document.body.prepend(div);
+  }
+}
+
