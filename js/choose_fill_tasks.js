d22f260439b49cd61cfddeaaf13879d25705588b
polinasam12 <sam53351@yandex.ru>
2021-07-26 01:24:21 +0300

    Поиск word в example вынесен в отдельную функцию

--- js/choose_fill_tasks.js
+++ js/choose_fill_tasks.js
@@ -74,6 +74,77 @@ function check_answer_on_click(pos_right, answers) {
     }
 }
 
+function search_word_in_example(word, s) {
+    let word_arr = word.split(" ");
+    let re = new RegExp("\\b" + word + "\\b", "i");
+    if (s.search(re) == -1) {
+        re = new RegExp("\\b" + word + "[a-z]+\\b", "i");
+    }
+    if ((s.search(re) == -1) && (word[word.length - 1] == "y")) {
+        re = new RegExp("\\b" + word.slice(0, word.length - 1) + "ied\\b", "i");
+    }
+    if (s.search(re) != -1) {
+        word = s.match(re)[0];
+        s = s.replace(re, "______");
+    } else {
+        let w1 = "";
+        for (let i = 0; i < word_arr.length - 1; i++) {
+            w1 += ("\\b" + word_arr[i] + "[a-z]*\\b|");
+            if (word_arr[i][word_arr[i].length - 1] == "y") {
+                w1 += ("\\b" + word_arr[i].slice(0, word_arr[i].length - 1) + "ied\\b|");
+            }
+        }
+        w1 += "\\b" + word_arr[word_arr.length - 1] + "[a-z]*\\b"
+        if (word_arr[word_arr.length - 1][word_arr[word_arr.length - 1].length - 1] == "y") {
+            w1 += ("|\\b" + word_arr[word_arr.length - 1].slice(0, word_arr[word_arr.length - 1].length - 1) + "ied\\b");
+        }
+        let re1 = new RegExp(w1, "gi");
+        let s_words = Array.from(s.matchAll(re1));
+        let s_words1 = [];
+        let corr_ans = "";
+        for (let i = 0; i < s_words.length - word_arr.length + 1; i++) {
+            let k = 0;
+            for (let j = 0; j < word_arr.length; j++) {
+                let w2 = "\\b" + word_arr[j] + "[a-z]*\\b";
+                if (word_arr[j][word_arr[j].length - 1] == "y") {
+                    w2 += ("|\\b" + word_arr[j].slice(0, word_arr[j].length - 1) + "ied\\b");
+                }
+                let re2 = new RegExp(w2, "i");
+                if (s_words[i + j][0].search(re2) != -1) {
+                    k++;
+                }
+            }
+            if (k == word_arr.length) {
+                for (let j = 0; j < word_arr.length; j++) {
+                    s_words1.push(s_words[i + j]);
+                    corr_ans += (s_words[i + j][0] + " ");
+                }
+            }
+        }
+        corr_ans = corr_ans.trim();
+        if (corr_ans == "") {
+            return [word, s];
+        }
+        let x = s_words1[0].index;
+        let y = s_words1[s_words1.length - 1].index + s_words1[s_words1.length - 1][0].length;
+        let s1 = s.slice(x, y);
+        s1 = s1.replace(re1, "______");
+        s = s.slice(0, x) + s1 + s.slice(y, s.length);
+        let re3 = /(______\s)+______/g;
+        s = s.replace(re3, "______");
+        word = corr_ans;
+    }
+    word = word.split(" ");
+    for (let i = 0; i < word.length; i++) {
+        word[i] = word[i].toLowerCase();
+        if (word_arr[i][0] == word_arr[i][0].toUpperCase()) {
+            word[i] = word[i][0].toUpperCase() + word[i].slice(1, word[i].length)
+        }
+    }
+    word = word.join(" ");
+    return [word, s];
+}
+
 function choose_task() {
     let ir = get_random(words.length - 1);
     if (lang1 == "ru") {
@@ -94,70 +165,9 @@ function fill_task() {
     let ir = get_random(words.length - 1);
     let word = words[ir].word.trim();
     let s = words[ir].examples[0].eng;
-    word_arr = word.split(" ");
-    let re = new RegExp("\\b" + word + "\\b", "i");
-    if (s.search(re) == -1) {
-        re = new RegExp("\\b" + word + "[a-z]+\\b", "i");
-    }
-    if ((s.search(re) == -1) && (word[word.length - 1] == "y")) {
-        re = new RegExp("\\b" + word.slice(0, word.length - 1) + "ied\\b", "i");
-    }
-    if (s.search(re) != -1) {
-        word = s.match(re)[0];
-        s = s.replace(re, "______");
-    } else {
-        w1 = "";
-        for (let i = 0; i < word_arr.length - 1; i++) {
-            w1 += ("\\b" + word_arr[i] + "[a-z]*\\b|");
-            if (word_arr[i][word_arr[i].length - 1] == "y") {
-                w1 += ("\\b" + word_arr[i].slice(0, word_arr[i].length - 1) + "ied\\b|");
-            }
-        }
-        w1 += "\\b" + word_arr[word_arr.length - 1] + "[a-z]*\\b"
-        if (word_arr[word_arr.length - 1][word_arr[word_arr.length - 1].length - 1] == "y") {
-            w1 += ("|\\b" + word_arr[word_arr.length - 1].slice(0, word_arr[word_arr.length - 1].length - 1) + "ied\\b");
-        }
-        re1 = new RegExp(w1, "gi");
-        s_words = Array.from(s.matchAll(re1));
-        s_words1 = [];
-        corr_ans = "";
-        for (let i = 0; i < s_words.length - word_arr.length + 1; i++) {
-            k = 0;
-            for (let j = 0; j < word_arr.length; j++) {
-                w2 = "\\b" + word_arr[j] + "[a-z]*\\b";
-                if (word_arr[j][word_arr[j].length - 1] == "y") {
-                    w2 += ("|\\b" + word_arr[j].slice(0, word_arr[j].length - 1) + "ied\\b");
-                }
-                let re2 = new RegExp(w2, "i");
-                if (s_words[i + j][0].search(re2) != -1) {
-                    k++;
-                }
-            }
-            if (k == word_arr.length) {
-                for (let j = 0; j < word_arr.length; j++) {
-                    s_words1.push(s_words[i + j]);
-                    corr_ans += (s_words[i + j][0] + " ");
-                }
-            }
-        }
-        corr_ans = corr_ans.trim();
-        let x = s_words1[0].index;
-        let y = s_words1[s_words1.length - 1].index + s_words1[s_words1.length - 1][0].length;
-        s1 = s.slice(x, y);
-        s1 = s1.replace(re1, "______");
-        s = s.slice(0, x) + s1 + s.slice(y, s.length);
-        re3 = /(______\s)+______/g;
-        s = s.replace(re3, "______");
-        word = corr_ans;
-    }
-    word = word.split(" ");
-    for (let i = 0; i < word.length; i++) {
-        word[i] = word[i].toLowerCase();
-        if (word_arr[i][0] == word_arr[i][0].toUpperCase()) {
-            word[i] = word[i][0].toUpperCase() + word[i].slice(1, word[i].length)
-        }
-    }
-    word = word.join(" ");
+	let a1 = search_word_in_example(word, s);
+	word = a1[0];
+	s = a1[1];
     write_exercise(s);
 	let a = make_answers_ind(ir);
 	let pos_right = a[0];
