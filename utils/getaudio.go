f9eabee33dba56973d1a69225aad7a1cd4cc51f4
emrzvv <e.mrzvv@gmail.com>
2021-07-29 15:27:25 +0300

    добавлен черновой вариант обработки случаев, когда слово может являться разными частями речи (запрос имеет другой url). нуждается в рефакторинге

--- utils/getaudio.go
+++ utils/getaudio.go
@@ -62,11 +62,11 @@ func prepareWordForRequest(word *string) (string, string) {
 // https://www.oxfordlearnersdictionaries.com/definition/english/word?q=word
 // https://www.oxfordlearnersdictionaries.com/definition/english/splitted-word?q=splitted+word
 
-func sendGetRequestOxfordDictionary(word *string, audioFolderPtr *string) {
+func sendGetRequestOxfordDictionary(word *string, pos *string, audioFolderPtr *string) {
 	pathVariable, queryVariable := prepareWordForRequest(word)
+	mainUrl := "https://www.oxfordlearnersdictionaries.com/definition/english/"
 
-	fmt.Println(pathVariable + "  " + queryVariable)
-	re, err := http.Get("https://www.oxfordlearnersdictionaries.com/definition/english/" + pathVariable + "?q=" + queryVariable)
+	re, err := http.Get(mainUrl + pathVariable + "?q=" + queryVariable)
 
 	if err != nil {
 		log.Panic(err)
@@ -75,24 +75,68 @@ func sendGetRequestOxfordDictionary(word *string, audioFolderPtr *string) {
 	defer re.Body.Close()
 
 	if re.StatusCode != 200 {
-		log.Println("bad status code")
-		log.Println(re.StatusCode)
-		log.Println(*word)
-		// try to bruteforce
-	} else {
-		doc, err := goquery.NewDocumentFromReader(re.Body)
+		reType1, err1 := http.Get(mainUrl + pathVariable + "_1?q=" + queryVariable)
 
+		if err1 != nil {
+			log.Println("bad request")
+			log.Println(reType1.StatusCode)
+		}
+
+		defer reType1.Body.Close()
+
+		reType2, err2 := http.Get(mainUrl + pathVariable + "_2?q=" + queryVariable)
+
+		if err2 != nil {
+			log.Println("bad request")
+			log.Println(reType2.StatusCode)
+		}
+
+		if err1 != nil && err2 != nil {
+			log.Println("No such word in oxfordlearnersdictionaries")
+		}
+
+		defer reType2.Body.Close()
+
+		doc1, err := goquery.NewDocumentFromReader(reType1.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		audioUrl, _ := doc.Find(".sound").First().Attr("data-src-mp3")
-		log.Println("downloaded " + audioUrl)
-		downloadErr := downloadFile(*audioFolderPtr+"/"+*word+".mp3", audioUrl)
-		if downloadErr != nil {
-			log.Println(downloadErr)
+		doc2, err := goquery.NewDocumentFromReader(reType2.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		type1 := doc1.Find(".pos").First().Text()
+		type2 := doc2.Find(".pos").First().Text()
+
+		if *pos == "n" {
+			if type1 == "noun" {
+				re, _ = http.Get(mainUrl + pathVariable + "_1?q=" + queryVariable)
+			} else if type2 == "noun" {
+				re, _ = http.Get(mainUrl + pathVariable + "_2?q=" + queryVariable)
+			}
+		} else if *pos == "v" {
+			if type1 == "verb" {
+				re, _ = http.Get(mainUrl + pathVariable + "_1?q=" + queryVariable)
+			} else if type2 == "verb" {
+				re, _ = http.Get(mainUrl + pathVariable + "_2?q=" + queryVariable)
+			}
 		}
 	}
+	doc, err := goquery.NewDocumentFromReader(re.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	audioUrl, _ := doc.Find(".sound").First().Attr("data-src-mp3")
+	log.Println("downloaded " + audioUrl)
+	downloadErr := downloadFile(*audioFolderPtr+"/"+*word+".mp3", audioUrl)
+	if downloadErr != nil {
+		log.Println(downloadErr)
+	}
+
 }
 
 func getAudioFromJson(jsonPath *string, audioFolderPath *string) {
@@ -109,7 +153,7 @@ func getAudioFromJson(jsonPath *string, audioFolderPath *string) {
 
 	for i := 0; i < len(entries.Entries); i++ {
 		if entries.Entries[i].Pos != "phr" {
-			sendGetRequestOxfordDictionary(&entries.Entries[i].Word, audioFolderPath)
+			sendGetRequestOxfordDictionary(&entries.Entries[i].Word, &entries.Entries[i].Pos, audioFolderPath)
 		}
 	}
 
@@ -133,6 +177,6 @@ func main() {
 		if pathErr != nil {
 			log.Panicln("Can't create folder for audio")
 		}
-		getAudioFromJson(wordsFilePtr, audioFolderPtr)
 	}
+	getAudioFromJson(wordsFilePtr, audioFolderPtr)
 }
