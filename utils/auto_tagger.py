bdf8ddee611ae3648102dd27f5c3ea945c94fc66
Kirill Soloshenko <smertlove@yandex.ru>
2021-08-06 00:14:04 +0300

    доработал лемматизацию английских слов. Функцию parse_entry разделил на две функции parse_entry_eng и parse_entry_ru для парсинга английских и русских примеров соответственно. Добавил глобальную переменную NLTK_WORDNET_POS_TAGS, содержащую кортеж wordnet-тегов из nltk.

--- utils/auto_tagger.py
+++ utils/auto_tagger.py
@@ -14,19 +14,14 @@ morph = pm.MorphAnalyzer()
 print('загрузка успешно завершена!\n---------------------------\n\n')
 
 
-NLTK_WORDNET_TAGS = {
-        
-        }
-##n 
-##adv
-##n
-##prep
-##a
-##v
-##phr
-##adj
-##n  
-##n v
+NLTK_WORDNET_POS_TAGS = (
+    wordnet.NOUN,
+    wordnet.VERB,
+    wordnet.ADJ,
+    wordnet.ADV
+    )
+
+
 
 def my_ngramm(string, n):
     string = nltk.word_tokenize(string)
@@ -68,40 +63,47 @@ def format_str(string, lang):
             string = string.replace(issues[letter],letter)
     return string
     
-def parse_entry(entry, file):    
-    err_eng = 0
-    err_ru = 0
+
+
+
+def parse_entry_eng(entry, file):
+    global NLTK_WORDNET_POS_TAGS
     logs = []
-    eng = [format_str(word, 'eng') for word in nltk.word_tokenize(entry['examples'][0]['eng'])]
-    ru  = [format_str(word, 'ru') for word in nltk.word_tokenize(entry['examples'][0]['ru'])]
-    eng_word_normalized = lemmatizer.lemmatize(format_str(entry['word'], 'eng').lstrip().rstrip().lower())
-    ru_word_normalized = [format_str(morph.parse(c)[0].normal_form, 'ru') for c in deal_with_brackets(parse_translation(entry['translation']))]
+    eng = [format_str(word, 'eng') for word in nltk.word_tokenize(entry['examples'][0]['eng'])]    
+    eng_word_normalized = lemmatizer.lemmatize(format_str(entry['word'], 'eng').lstrip().rstrip().lower(), entry['pos'].rstrip()[0])
     for word in eng:
         logs += [f'-----   {repr(lemmatizer.lemmatize(word))}, {repr(eng_word_normalized)}, {lemmatizer.lemmatize(word) == eng_word_normalized}   -----']
-        if lemmatizer.lemmatize(word) == eng_word_normalized:
-            file.write(f"Word \"{word}\" found\nin \"{' '.join(eng)}\"\nin {eng.index(word)+1} position.\n")
-            break
+        for tag in NLTK_WORDNET_POS_TAGS:
+            if lemmatizer.lemmatize(word, tag) == eng_word_normalized:
+                file.write(f"Word \"{word}\" found\nin \"{' '.join(eng)}\"\nin {eng.index(word)+1} position.\n")
+                return 0
     else:
         file.write( f"ERROR: Word \"{entry['word']}\" not found\nin {' '.join(eng)}.\n" )
-        err_eng+=1
-        for c in logs:
-            file.write(c)
-            file.write('\n')
-        logs = []
-    for word in ru:
-        for parse in morph.parse(word):
-            if parse.normal_form in ru_word_normalized:
-                file.write(f"Translation \"{word}\" found\nin \"{' '.join(ru)}\"\nin {ru.index(word)+1} position.\n")
-                return (err_eng, err_ru)
-            logs += [f'-----   {repr(parse.normal_form)}, {ru_word_normalized}, {parse.normal_form in ru_word_normalized}   -----']
-    else:
-        file.write(f"ERROR: Translation \"{entry['translation']}\" not found\nin {' '.join(ru)}.\n")
-        err_ru+=1
     for c in logs:
         file.write(c)
         file.write('\n')
-    
-    return (err_eng, err_ru)
+    return 1
+
+
+
+def parse_entry_ru(entry, file):
+    logs = []
+    ru  = [format_str(word, 'ru') for word in nltk.word_tokenize(entry['examples'][0]['ru'])]
+    ru_word_normalized = [format_str(morph.parse(c)[0].normal_form, 'ru') for c in deal_with_brackets(parse_translation(entry['translation']))]
+    for word in ru:
+        for parse in morph.parse(word):
+            logs += [f'-----   {repr(parse.normal_form)}, {ru_word_normalized}, {parse.normal_form in ru_word_normalized}   -----']
+            if parse.normal_form in ru_word_normalized:
+                file.write(f"Translation \"{word}\" found\nin \"{' '.join(ru)}\"\nin {ru.index(word)+1} position.\n")
+                return 0
+            
+    else:
+        file.write(f"ERROR: Translation \"{entry['translation']}\" not found\nin {' '.join(ru)}.\n")
+    for c in logs:
+        file.write(c)
+        file.write('\n')
+    return 1
+
 
 
 
@@ -110,10 +112,11 @@ def main():
     file = open('logs.txt', 'w', encoding = 'utf-8')
     a = []
     for entry in get_json_obj('words.json')['entries']:
-        if entry['pos'] not in a:
-        cur = parse_entry(entry, file)
-        total[0]+=cur[0]
-        total[1]+=cur[1]
+        try:
+            total[0]+=parse_entry_eng(entry, file)
+            total[1]+=parse_entry_ru(entry, file)
+        except Exception:
+            file.write(f'WORDNET ERROR: tag {entry["pos"][0]}({entry["pos"]}) not found.')
         file.write('\n- - - - - - - - - - - - - - - - -\n\n')
     file.close()
     print(f'всего ошибок:\nв английском {total[0]},\nв русском {total[1]}.')
