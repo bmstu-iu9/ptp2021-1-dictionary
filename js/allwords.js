131508ffe4b92caae781589cc879316967f83568
euphr0syne <generic200379@gmail.com>
2021-08-09 13:03:16 +0300

    now search by first letter works

--- js/allwords.js
+++ js/allwords.js
@@ -1,7 +1,11 @@
 var i = 0;
-var j = 0;
+var j = "z";
 //document.write('<tr>');
 for (i in words) {
+	if (j != words[i].word[0]) {
+		j = words[i].word[0];
+		document.write('<td><a name="' + j + '" href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
+	}
     document.write('<td><a href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
 }
 //document.write('</tr');
