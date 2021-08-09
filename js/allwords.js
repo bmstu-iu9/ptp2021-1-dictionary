c1fc70391f9ca3067b48867eacde68f1fe1be58c
euphr0syne <generic200379@gmail.com>
2021-08-09 14:37:48 +0300

    fixed allwords.js

--- js/allwords.js
+++ js/allwords.js
@@ -6,7 +6,9 @@ for (i in words) {
 		j = words[i].word[0];
 		document.write('<td><a name="' + j + '" href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
 	}
-    document.write('<td><a href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
+	else {
+		document.write('<td><a href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
+	}
 }
 //document.write('</tr');
 
