3e6811c7209b5a8ca0fec32655cf649883c86c6e
Lasadaf <86784572+Lasadaf@users.noreply.github.com>
2021-08-26 20:06:43 +0300

    показывает модули

--- js/allwords.js
+++ js/allwords.js
@@ -4,7 +4,7 @@ var j = "z";
 for (i in words) {
 	if (j != words[i].word[0]) {
 		j = words[i].word[0];
-		document.write('<td><a name="' + j + '" href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
+		document.write('<td><a name="' + j + '" href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ', Модуль ' + words[i].module + ')</a></td>');
 	}
 	else {
 		document.write('<td><a href="pages/wordpage.html?wordnumber=' + i + '" title="Показать информацию об этом слове">' + words[i].word + ' (' + words[i].pos + ')</a></td>');
